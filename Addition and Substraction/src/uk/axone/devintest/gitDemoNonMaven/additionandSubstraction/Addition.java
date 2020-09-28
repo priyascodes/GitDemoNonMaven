f5e88a125c0741b9013642d318a0b289a4169c92
Priya <priya3kr@gmail.com>
2020-09-28 14:18:18 +0100

    Comitting Addition and Sunstraction.javafiles

---
+++ Addition and Substraction/src/uk/axone/devintest/gitDemoNonMaven/additionandSubstraction/Addition.java
@@ -0,0 +1,11 @@
+package uk.axone.devintest.gitDemoNonMaven.additionandSubstraction;
+
+public class Addition  {
+
+    public int multiplyNumbers(int x, int y){
+
+        int z = x + y;
+        return z;
+
+    }
+}
---
+++ Addition and Substraction/src/uk/axone/devintest/gitDemoNonMaven/additionandSubstraction/Substraction.java
@@ -0,0 +1,11 @@
+package uk.axone.devintest.gitDemoNonMaven.additionandSubstraction;
+
+public class Substraction {
+
+    public int substractNumbers(int x, int y){
+
+        int z = x - y;
+        return z;
+
+    }
+}
